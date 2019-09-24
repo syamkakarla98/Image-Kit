9ad905d503e2eed5d5563315400952264108bf94
syamkakarla98 <syamkakarla98@gmail.com>
2019-09-24 14:29:19 +0530

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Image-Kit',
+    url='https://github.com/syamkakarla98/Image-Kit',
+    author='Syam Kakarla',
+    author_email='Syamkakarla98@gmail.com',
+    # Needed to actually package something
+    packages=['ImageKit'],
+    # Needed for dependencies
+    install_requires=['numpy', 'matplotlib', 'Pillow', 'scipy'],
+    # *strongly* suggested for sharing
+    version='1.0.0',
+    # The license can be anything you like
+    license='MIT',
+    description='A module for basic image processing.',
+    long_description=open('README.txt').read(),
+)
